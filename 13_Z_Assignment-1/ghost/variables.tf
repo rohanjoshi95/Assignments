0a96e568cbf50df68e2e4c5003645c6c886fb258
Joshi <rohan.joshi@capgemini.com>
2020-06-09 17:18:42 +0530

    terraform assignments

---
+++ 13_Z_Assignment-1/ghost/variables.tf
@@ -0,0 +1,9 @@
+variable "image_name" {
+  description = "Image for container"
+}
+variable "container_name" {
+  description = "Name of the container"
+}
+variable "ext_port" {
+  description = "External port for container"
+}
